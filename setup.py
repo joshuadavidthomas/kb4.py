3e8259f0ae8648eece8d8ff75ba0c924950ad692
josh <josh@joshuadavidthomas.com>
2019-04-16 18:43:16 -0500

    Add initial setup.py
    Not really needed yet, but I figured I better get a good start on best
    practices of Python development and I have yet to see a project that
    does not have one of these.

---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+
+setup(
+  name='knowbe4.py',
+  version='0.0.1',
+  description='A simple Python wrapper for the KnowBe4 API',
+  author='Josh Thomas',
+  author_email='josh@joshuadavidthomas.com',
+  install_requires=[
+    'requests>=2.21.0',
+  ],
+  tests_require=[
+    'pytest',
+  ],
+  license='LICENSE.md',
+  packages=['kb4']
+)
