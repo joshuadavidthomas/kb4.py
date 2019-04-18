d69eb7c0c5ecf975b16186e6e89a20427f56f6a8
josh <git@joshuadavidthomas.com>
2019-04-18 10:38:28 -0500

    Add spacing to conform to PEP008

--- kb4/exceptions.py
+++ kb4/exceptions.py
@@ -1,17 +1,19 @@
 class KnowBe4Exception(Exception):
-  pass
+    pass
+
 
 class ConfigurationError(KnowBe4Exception):
-  def __init__(self, msg):
-    self.msg = msg
+    def __init__(self, msg):
+        self.msg = msg
 
-  def __str__(self):
-    return self.msg
+    def __str__(self):
+        return self.msg
+
 
 class HTTPError(KnowBe4Exception):
-  def __init__(self, message, code):
-    self.status_code = code
-    self.msg = '{}: {}'.format(code, message)
+    def __init__(self, message, code):
+        self.status_code = code
+        self.msg = '{}: {}'.format(code, message)
 
-  def __str__(self):
-    return self.msg
+    def __str__(self):
+        return self.msg
--- setup.py
+++ setup.py
@@ -10,29 +10,29 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-  name='knowbe4.py',
-  version=version,
-  description='A simple Python wrapper for the KnowBe4 API',
-  long_description=long_description,
-  long_description_content_type="text/markdown",
-  author='Josh Thomas',
-  author_email='josh@joshuadavidthomas.com',
-  url='https://github.com/elderlydoofus/knowbe4.py',
-  install_requires=[
-    'requests>=2.21.0',
-    'python-dotenv>=0.10.1',
-  ],
-  tests_require=[
-    'pytest',
-    'vcrpy',
-  ],
-  license='LICENSE.md',
-  packages=['kb4'],
-  classifiers=[
-    'Development Status :: 2 - Pre-Alpha',
-    'Programming Language :: Python :: 3',
-    'Programming Language :: Python :: 3.6',
-    'License :: OSI Approved :: MIT License',
-    'Operating System :: OS Independent',
+    name='knowbe4.py',
+    version=version,
+    description='A simple Python wrapper for the KnowBe4 API',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author='Josh Thomas',
+    author_email='josh@joshuadavidthomas.com',
+    url='https://github.com/elderlydoofus/knowbe4.py',
+    install_requires=[
+        'requests>=2.21.0',
+        'python-dotenv>=0.10.1',
+    ],
+    tests_require=[
+        'pytest',
+        'vcrpy',
+    ],
+    license='LICENSE.md',
+    packages=['kb4'],
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
     ],
 )
